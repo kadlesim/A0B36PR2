ecf8a86aa9c0d4fe5a29a725840e6c9fb4dc2fd5
Simon <Simon@SimonX1>
2013-03-20 15:20:32 +0100

    neco k zadani

--- Semestralka/src/semestralka/Semestralka.java
+++ Semestralka/src/semestralka/Semestralka.java
@@ -14,6 +14,7 @@ public class Semestralka {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        dsadsdasd
+        System.out.println("Login: ");
+        System.out.println("zadej ip adresu druheo uzivatele");
     }
 }
